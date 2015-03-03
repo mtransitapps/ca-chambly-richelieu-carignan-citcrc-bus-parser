fb48cf63b96e574e2ac78d17167649e8e429c70c
Mathieu Méa <mathieu.mea@gmail.com>
2015-03-03 16:31:37 -0500

    Generate agency values XML file.

--- src/org/mtransit/parser/ca_chambly_richelieu_carignan_citcrc_bus/ChamblyRichelieuCarignanCITCRCBusAgencyTools.java
+++ src/org/mtransit/parser/ca_chambly_richelieu_carignan_citcrc_bus/ChamblyRichelieuCarignanCITCRCBusAgencyTools.java
@@ -11,6 +11,7 @@ import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
+import org.mtransit.parser.mt.data.MAgency;
 import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.mt.data.MSpec;
 import org.mtransit.parser.mt.data.MTrip;
@@ -18,8 +19,6 @@ import org.mtransit.parser.mt.data.MTrip;
 // https://www.amt.qc.ca/en/about/open-data
 // http://www.amt.qc.ca/xdata/citcrc/google_transit.zip
 public class ChamblyRichelieuCarignanCITCRCBusAgencyTools extends DefaultAgencyTools {
-
-	public static final String ROUTE_TYPE_FILTER = "3"; // bus only
 
 	public static void main(String[] args) {
 		if (args == null || args.length == 0) {
@@ -67,11 +66,8 @@ public class ChamblyRichelieuCarignanCITCRCBusAgencyTools extends DefaultAgencyT
 	}
 
 	@Override
-	public boolean excludeRoute(GRoute gRoute) {
-		if (ROUTE_TYPE_FILTER != null && !gRoute.route_type.equals(ROUTE_TYPE_FILTER)) {
-			return true;
-		}
-		return super.excludeRoute(gRoute);
+	public Integer getAgencyRouteType() {
+		return MAgency.ROUTE_TYPE_BUS;
 	}
 
 	@Override
